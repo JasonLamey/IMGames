75069e8e9d9cffc069e3e6b87280be3ef0f480b1
Jason Lamey <jasonlamey@gmail.com>
2016-10-11 16:52:15 -0400

    Instituted basic sign-up, including duplicate account verification.

--- lib/IMGames.pm
+++ lib/IMGames.pm
@@ -71,43 +71,91 @@ post '/signup' => sub {
 
   if ( $form_results->has_invalid or $form_results->has_missing )
   {
-    deferred( error => "Errors have occurred in your sign up information." );
+    my @errors = ();
+    for my $invalid ( $form_results->invalid )
+    {
+      push( @errors, sprintf( "<strong>%s</strong> is invalid: %s<br>", $invalid, $form_results->invalid( $invalid ) ) );
+    }
+
+    for my $missing ( $form_results->missing )
+    {
+      push( @errors, sprintf( "<strong>%s</strong> needs to be filled out.<br>", $missing ) );
+    }
+
+    deferred( error => sprintf( "Errors have occurred in your sign up information.<br>%s", join( '<br>', @errors ) ) );
     redirect '/';
   }
-  else
+
+  my $user_check = $SCHEMA->resultset( 'User' )->find( { username => body_parameters->get( 'username' ) } );
+
+  if (
+      defined $user_check
+      &&
+      ref( $user_check ) eq 'IMGames::Schema::Result::User'
+      &&
+      $user_check->username eq body_parameters->get( 'username' )
+     )
   {
-    my $now = DateTime->now( time_zone => 'UTC' );
-    my $new_user = $SCHEMA->resultset( 'User' )->new(
-                                                      {
-                                                        username   => body_parameters->get( 'username' ),
-                                                        password   => body_parameters->get( 'password' ),
-                                                        email      => body_parameters->get( 'email' ),
-                                                        birthdate  => body_parameters->get( 'birthdate' ),
-                                                        confirmed  => 0,
-                                                        created_on => $now,
-                                                      }
-                                                    );
-    $SCHEMA->txn_do(
-                    sub
-                    {
-                      $new_user->insert;
-                    }
-    );
-
-    my $unconfirmed_role = $SCHEMA->resultset( 'Role' )->find( { role => 'Unconfirmed' } );
-
-    debug "About to set role";
-    $new_user->set_roles( $unconfirmed_role );
-    debug "Just set role";
-
-    info sprintf( 'Created new user >%s<, ID: >%s<, on %s', $new_user->username, $new_user->id, $now );
-
-    deferred( success => sprintf("Thanks for signing up, %s!", body_parameters->get( 'username' ) ) );
-    session 'user'      => body_parameters->get( 'username' );
-    session 'logged_in' => 1;
-    session->expires( $USER_SESSION_EXPIRE_TIME );
-    redirect '/signed_up';
+    deferred error => sprintf( 'Username <strong>%s</strong> is already in use.', body_parameters->get( 'username' ) );
+    redirect '/';
   }
+
+  my $email_check = $SCHEMA->resultset( 'User' )->find( { email => body_parameters->get( 'email' ) } );
+
+  if (
+      defined $email_check
+      &&
+      ref( $email_check ) eq 'IMGames::Schema::Result::User'
+      &&
+      $email_check->email eq body_parameters->get( 'email' )
+     )
+  {
+    deferred error => sprintf( 'There is already an account associated to the email address <strong>%s</strong>.', body_parameters->get( 'email' ) );
+    redirect '/';
+  }
+
+  my $now = DateTime->now( time_zone => 'UTC' );
+  my $new_user = $SCHEMA->resultset( 'User' )->new(
+                                                    {
+                                                      username   => body_parameters->get( 'username' ),
+                                                      password   => body_parameters->get( 'password' ),
+                                                      email      => body_parameters->get( 'email' ),
+                                                      birthdate  => body_parameters->get( 'birthdate' ),
+                                                      confirmed  => 0,
+                                                      created_on => $now,
+                                                    }
+                                                  );
+  $SCHEMA->txn_do(
+                  sub
+                  {
+                    $new_user->insert;
+                  }
+  );
+
+  my $unconfirmed_role = $SCHEMA->resultset( 'Role' )->find( { role => 'Unconfirmed' } );
+
+  my $user_role = $SCHEMA->resultset( 'UserRole' )->new(
+                                                        {
+                                                          user_id => $new_user->id,
+                                                          role_id => $unconfirmed_role->id,
+                                                        }
+                                                       );
+  $SCHEMA->txn_do(
+                  sub
+                  {
+                    $user_role->insert;
+                  }
+  );
+
+  info sprintf( 'Created new user >%s<, ID: >%s<, on %s', $new_user->username, $new_user->id, $now );
+
+  # Email confirmation message to the user.
+
+  deferred( success => sprintf("Thanks for signing up, %s!", body_parameters->get( 'username' ) ) );
+  session 'user'      => body_parameters->get( 'username' );
+  session 'logged_in' => 1;
+  session->expires( $USER_SESSION_EXPIRE_TIME );
+  redirect '/signed_up';
 };
 
 
@@ -121,15 +169,17 @@ get '/signed_up' => sub {
 
   if ( ! session( 'logged_in' ) )
   {
+    info 'An anonymous (not logged in) user attempted to access /signed_up.';
     deferred error => 'You need to be logged in to access that page.';
     redirect '/login';
   }
 
   my $username = session( 'user' );
-  my $user = $SCHEMA->resultset('User')->search( { username => $username } );
+  my $user = $SCHEMA->resultset( 'User' )->find( { username => $username } );
 
   if ( ref( $user ) ne 'IMGames::Schema::Result::User' )
   {
+    warning sprintf( 'A user (%s) attempted to reach /signed_up, but the account could not be confirmed to exist.', session( 'user' ) );
     deferred error => 'You need to be logged in to access that page.';
     redirect '/login';
   }
@@ -139,6 +189,29 @@ get '/signed_up' => sub {
                                             user => $user,
                                           },
                                 };
+};
+
+
+=head2 GET C</login>
+
+Login page for redirection, login errors, reattempt, etc.
+
+=cut
+
+get '/login' => sub {
+  template 'login';
+};
+
+
+=head2 ANY C</logout>
+
+Logout route, for killing user sessions, and redirecting to the index page.
+
+=cut
+
+any '/logout' => sub {
+  app->destroy_session;
+  template 'logout';
 };
 
 
